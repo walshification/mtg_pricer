c4e16b2d28a3b03c88f5685d869d15663555c8d1
walshification <walshification@gmail.com>
2014-11-20 19:04:11 -0600

    added mtg_pricer class in module for rails apps

--- lib/mtg_pricer.rb
+++ lib/mtg_pricer.rb
@@ -1,5 +1,24 @@
 require "mtg_pricer/version"
 
 module MtgPricer
-  # Your code goes here...
+  class MtGPricer
+    attr_accessor :card, :set
+
+    def initialize(options = {})
+      @card = options[] || ''
+      @set = options[] || ''
+    end
+
+    def tcgplayer_price(card, set)
+      @card = Unirest.get("http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname=#{card}&cardset=#{set}").body
+    end
+
+    def cfg_price(card, set)
+      @card = Unirest.get("http://magictcgprices.appspot.com/api/cfg/price.json?cardname=#{card}&cardset=#{set}").body
+    end
+
+    def ebay_price(card, set)
+      @card = Unirest.get("http://magictcgprices.appspot.com/api/ebay/price.json?cardname=#{card}&cardset=#{set}").body
+    end
+  end
 end
